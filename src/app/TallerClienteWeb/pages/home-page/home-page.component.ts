2de1a79daaccb35f6d8de3dac59b6ab88188b385
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-18 11:48:58 -0300

    feat: pagina inicio y ruta a ella creada

---
+++ src/app/TallerClienteWeb/pages/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-home-page',
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () => import('./TallerClienteWeb/pages/home-page/home-page.component').then(m => m.HomePageComponent)
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    }
+];
