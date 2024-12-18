86f4c143bb105ac8361233d3d72e8cf330955af9
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-18 12:25:50 -0300

    feat: botones inicio sesion y registrarse creados y puestos en home page

--- src/app/TallerClienteWeb/pages/home-page/home-page.component.ts
+++ src/app/TallerClienteWeb/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginButtonComponent } from '../../components/login-button/login-button.component';
+import { RegisterButtonComponent } from '../../components/register-button/register-button.component';
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [LoginButtonComponent, RegisterButtonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
